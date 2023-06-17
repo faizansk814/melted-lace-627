ec8300e25928a177b4d716056e8afd9573a3a0f0
AAdi23456 <118712556+AAdi23456@users.noreply.github.com>
2023-06-18 03:26:35 +0530

    payment gateway work is done

--- Backend/index.js
+++ Backend/index.js
@@ -1,27 +1,33 @@
 const express = require("express")
 const cors = require("cors")
 require("dotenv").config()
-const {BookingRoute}=require("./routes/Booking")
-const{DataBase}=require("./DBconnection")
-const UserModel = require("./model/user.model")
+const port = process.env.PORT
+const { BookingRoute } = require("./routes/Booking")
+const { DataBase } = require("./DBconnection")
+const { Payment } = require("./routes/payment")
 const userrouter = require("./routes/user.router")
 const doctorroute = require("./routes/doctor.route")
 const auth = require("./middleware/auth")
-const app=express()
+const app = express()
+const bodyParser = require('body-parser');
 app.use(express.json())
 app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/user", userrouter)
+app.use("/doctor", doctorroute)
+app.use("/appointment", BookingRoute)
+app.use("/petcare", Payment)
 
-app.use("/user",userrouter);
-app.use("/doctor",doctorroute)
-app.use(auth)
-app.use("/appointment",BookingRoute)
 
-app.listen(process.env.PORT, () => {
+app.listen(port, () => {
     try {
         DataBase()
-        console.log(`Server is running on port${process.env.PORT}`)
+        console.log(`Server is running on port${port}`)
     } catch (error) {
         console.error(error)
     }
 
 })
+
+
