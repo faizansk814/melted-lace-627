8e0e918c0590e4187dde171a831060c995062e21
faizansk <faizansk814@gmail.com>
2023-06-17 22:07:06 +0530

    conflict fixed

--- Backend/index.js
+++ Backend/index.js
@@ -5,12 +5,15 @@ const {BookingRoute}=require("./routes/Booking")
 const{DataBase}=require("./DBconnection")
 const UserModel = require("./model/user.model")
 const userrouter = require("./routes/user.router")
+const doctorroute = require("./routes/doctor.route")
+const auth = require("./middleware/auth")
 const app=express()
 app.use(express.json())
 app.use(cors())
 
 app.use("/user",userrouter);
-
+app.use("/doctor",doctorroute)
+app.use(auth)
 app.use("/appointment",BookingRoute)
 
 app.listen(process.env.PORT, () => {
--- Backend/model/user.model.js
+++ Backend/model/user.model.js
@@ -1,14 +1,16 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose')
 
-const userSchema=mongoose.Schema({
-    name:{type:String,required:true},
-    email:{type:String,required:true},
-    password:{type:String,required:true},
-    isVerified:{type:Boolean,default:false},}
-    
- )
+const userSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    isVerified: { type: Boolean, default: false },
+    role: { type: String, default: "user", enum: ["admin", "user"] }
+}
+
+)
 
 
-const UserModel=mongoose.model("user",userSchema)
+const UserModel = mongoose.model("user", userSchema)
 
-module.exports=UserModel
+module.exports = UserModel
