a9a8c11135fd48be2425e8b50feea919d66af8db
Anshita <anshitagupta783@gmail.com>
2023-06-17 19:59:35 +0530

    done

--- Backend/index.js
+++ Backend/index.js
@@ -5,40 +5,18 @@ require("dotenv").config()
 const {BookingRoute}=require("./routes/Booking")
 const{DataBase}=require("./DBconnection")
 const userrouter = require("./routes/user.router")
-const path = require("path");
 const UserModel = require("./model/user.model")
-app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json())
 app.use(cors())
 
-app.get("/verify", async (req, res) => {
-    try {
-      const userId = req.query.id;
-  
-      const user = await UserModel.updateOne(
-        { _id: userId },
-        {$set:{isVerified:true}}
-      );
-     
-      
-      if (user.isVerified) {
-        return res.status(200).json({ message: "Email already verified" });
-      }
-  
-      res.sendFile(path.join(__dirname, "public", "pages", "verify.html"));
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ error: "Internal server error" });
-    }
-  });
 
 app.use("/user",userrouter);
 
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
     try {
         DataBase()
-        console.log(`Server is running on port${port}`)
+        console.log(`Server is running on port${process.env.PORT}`)
     } catch (error) {
         console.error(error)
     }
--- Backend/routes/user.router.js
+++ Backend/routes/user.router.js
@@ -3,55 +3,68 @@ const nodemailer = require("nodemailer")
 const UserModel = require('../model/user.model')
 const userrouter = express.Router()
 const bcrypt = require('bcrypt')
+const app = express()
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
+
+
 const jwt = require('jsonwebtoken')
 const { BlacklistModel } = require('../Google_Oauth/models/blacklist')
 
+
 const sendVerificationMail = async (name, email, userId) => {
-    try {
-      const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,
-        secure: false,
-        requireTLS: true,
-        auth: {
-          user: "anshita674@gmail.com",
-          pass: "decehtqforbxpjza",
-        },
-      });
-  
-      const mailOptions = {
-        from: "anshita674@gmail.com",
-        to: email,
-        subject: "For verification mail",
-        html:` <p>Hi ${name}, please click here to <a href="http://localhost:8080/verify?id=${userId}">verify</a>your mail</p>`,
-      };
-  
-      transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          console.error("Error sending email:", error);
-        } else {
-          console.log("Email sent:", info.response);
-        }
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      requireTLS: true,
+      auth: {
+        user: "anshita674@gmail.com",
+        pass: "decehtqforbxpjza",
+      },
+    });
+
+    const mailOptions = {
+      from: "anshita674@gmail.com",
+      to: email,
+      subject: "For verification mail",
+      html: `<p>Hi ${name}, please click here to <a href="http://localhost:8080/user/verify?id=${userId}">verify</a> your mail</p>`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error("Error sending email:", error);
+      } else {
+        console.log("Email sent:", info.response);
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 userrouter.post("/register", async (req, res) => {
-    const { name, email, gender, password } = req.body
     try {
-        const isUserPresent = await UserModel.findOne({ email })
-        if (isUserPresent) {
-            return res.status(401).send({ msg: "User Already Present" })
-        }
-        bcrypt.hash(password, 5, async (err, hash) => {
-            const newUser = new UserModel({ name, email, gender, password: hash })
-            await newUser.save()
-            return res.status(200).send({ msg: "Registration Succesful" })
-        })
-
-
+      const { name, email, password } = req.body;
+  
+      const userExist = await UserModel.findOne({ email });
+  
+      if (userExist) {
+        return res.status(401).send({ msg: "User Already Registered" });
+      }
+  
+      const hash = await bcrypt.hash(password, 8);
+  
+      const newUser = new UserModel({ name, email, password: hash });
+  
+      const userData = await newUser.save();
+      if (userData) {
+        sendVerificationMail(name, email, userData._id);
+        res.status(200).json({ msg: "Registration successful", userData });
+      } else {
+        res.status(401).json({ msg: "Registration failed" });
+      }
     } catch (error) {
       res.status(400).json({ msg: "Something went wrong" });
     }
@@ -88,6 +101,31 @@ userrouter.post("/register", async (req, res) => {
       res.status(401).send(error.message);
     }
   });
+
+  userrouter.get("/verify", async (req, res) => {
+    try {
+      const userId = req.query.id;
+  
+      const user = await UserModel.updateOne(
+        { _id: userId },
+        {$set:{isVerified:true}}
+      );
+      if (!user) {
+        return res
+          .status(404)
+          .json({ error: "User not found" });
+      }
+  
+      if (user.isVerified) {
+        return res.status(200).json({ message: "Email already verified" });
+      }
+  
+      res.sendFile(path.join(__dirname, "../public/pages/verify.html"));
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  });
   
   
   userrouter.get("/logout", async (req, res) => {
@@ -104,13 +142,14 @@ userrouter.post("/register", async (req, res) => {
 
 
 
-userrouter.get("/userget", async (req, res) => {
-    try {
-        const allDoctors = await UserModel.find()
-        return res.status(200).send({ allDoctors })
-    } catch (error) {
-        return res.status(401).send({ msg: error.message })
-    }
+userrouter.delete("/delete/:id",async (req,res)=>{
+    const {id}=req.params
+    const deleteUsers=await UserModel.findByIdAndDelete({_id:id})
+    return res.status(200).send({msg:"User Deleted"})
 })
+
+
+
+
 
 module.exports=userrouter
